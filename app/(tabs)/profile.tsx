4046ab5073c8949307a78c5163826f48420fe50d
Chrisagon <christophe.thomas@fesc.asso.fr>
2025-07-26 17:49:43 +0200

    Corriger les espaces indésirables dans le formulaire de connexion

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -96,7 +96,6 @@ export default function ProfileScreen() {
               onChangeText={setLoginPassword}
               secureTextEntry
             />
-            
             {isRegistering && (
               <TextInput
                 style={styles.loginInput}
